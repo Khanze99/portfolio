cd8cbb7cfac3edc72f8a49d6c7bd2b49be504954
Khanze99 <khamidov_anvar@mail.ru>
2024-06-14 04:39:29 +0300

    KHNZ-11. Добавление модели и модель в админку

--- khnz/core/admin.py
+++ khnz/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import ServiceRequest
+
 # Register your models here.
+
+
+@admin.register(ServiceRequest)
+class ServiceRequestAdmin(admin.ModelAdmin):
+    pass
--- khnz/core/models.py
+++ khnz/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+
+class ServiceRequest(models.Model):
+    customer_request_name = models.CharField(max_length=50)
+    email_customer = models.EmailField(max_length=254, unique=True)
+    customer_phone_number = PhoneNumberField(unique=True, null=True, blank=True)
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+
+    def __str__(self):
+        return f'{self.title} - {self.email_customer}'
+
+    class Meta:
+        verbose_name = 'Service Request'
+        verbose_name_plural = 'Service Requests'
