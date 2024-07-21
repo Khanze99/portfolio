b5a50db248c7126d5b8cc471860d911495129f6c
Khanze99 <khamidov_anvar@mail.ru>
2024-07-21 06:46:35 +0300

    связать с nginx приложение

--- khnz/khnz/urls.py
+++ khnz/khnz/urls.py
@@ -36,8 +36,8 @@ schema_view = get_schema_view(
 
 urlpatterns_swagger = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('core/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('core/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 urlpatterns += urlpatterns_swagger
